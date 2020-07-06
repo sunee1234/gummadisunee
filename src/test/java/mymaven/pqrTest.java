b2a0991ea0c669583d547255657ecc73168c5941
suneeths <suneegummadi99@gmail.com>
2020-07-06 16:58:28 +0530

    my first code to github

---
+++ src/test/java/mymaven/pqrTest.java
@@ -0,0 +1,22 @@
+package mymaven;
+
+import org.testng.annotations.Test;
+
+public class pqrTest 
+
+{
+	
+	
+	@Test
+	public void methodOne()
+	{
+		System.out.println("simple methodOne");
+	}
+	
+	@Test
+	public void methodTwo()
+	{
+		System.out.println("simple methodTwo");
+	}
+
+}
